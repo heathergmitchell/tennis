87f84a98b95beb48074027395e521f3e3add5f67
Emily Sarro <sarroen19@juniata.edu>
2023-04-18 11:37:24 -0400

    adding deuce, but inf loop

--- tennis_fix.py
+++ tennis_fix.py
@@ -28,13 +28,53 @@ if __name__ == '__main__':
 	scoreA = 0
 	scoreB = 0
 
+	deuceA = 0
+	deuceB = 0
+
 	score = ['Love', '15', '30', '40', '60']
 	sequence = max(scoreA,scoreB)
+	fortyall = ['Deuce', 'Ad-In', 'Ad-Out']
+
 
 	while (sequence < 4):
 		print(point)
+		if( scoreA ==3 and scoreB ==3):
+			while(deuceA != 2 or deuceB != 2):
+				if(counter % 2 == 0):   #Player A serving
+					if(point <= 50):	#Player A won the point
+						deuceA +=1
+						print(lineup[0] + " vs. " + lineup[1])
+						print("Advantage In")
+					else: #Player A lost point
+						deuceB += 1
+						print(lineup[0] + " vs. " + lineup[1])
+						print("Advantage Out")
+						if(deuceA != 0):
+							deuceA -=1
+				else:   #Player B serving
+					if(point > 50):      #Player B won point
+						deuceB += 1
+						print(lineup[0] + " vs. " + lineup[1])
+						print("Advantage In")
+					else:   #Player B lost point
+						deuceA +=1
+						print(lineup[0] + " vs. " +  lineup[1])
+						print("Advantage Out")
+						if (deuceB != 0):
+							deuceB -= 1
 
-		if(point <= 50):
+				if(deuceA == 2):
+					print(point)
+					print(lineup[0] + " won first game")
+					print("Ad vs. " + score[scoreB])
+				elif(deuceB == 2):
+					print(point)
+					print(lineup[1] + " won first game")
+					print(score[scoreA] + " vs. Ad")
+
+				point = random.randint(0,100)
+				print(point)
+		elif(point <= 50):
 			print("Player A wins the point")
 			scoreA += 1
 		else:
